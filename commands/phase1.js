4128d33c24ffb76c51f96f8e27e9680ab75ea819
XxShadowsmartxX <xxshadowsmartxx@gmail.com>
2020-07-29 16:18:52 -0500

    Update phase1.js

--- commands/phase1.js
+++ commands/phase1.js
@@ -8,7 +8,7 @@ module.exports = {
         if (message.member.roles.cache.has('716371210023338027')){
         const embed = new Discord.MessageEmbed()
             .setColor('020f73')
-            .setTitle('Phase 1 Announcement')
+            .setTitle('Phase 1 gay')
             .setAuthor('Aigio Law Enforcement Training Academy', 'https://media.discordapp.net/attachments/705093565113434212/736015262477844500/6c1e7537f9aa230b0a49494c49779dca.png')
             .setDescription(`PHASE # BEGINNING SHORTLY!\n\nA **Phase 1** is being hosted by, ${message.author}. Come on down!\n\nPhase Slock At: **${args[0]} ${args[1]} ${args[2]}**\n\nFollow all directions posted at the campus and be prepared!\n\n**Campus Link**: https://www.roblox.com/games/5046852624/ALETA-Campus?refPageId=703da718-c993-4ed5-b154-4b2f3017c2ba#`);
         message.channel.send(embed);
