fb282bccc34cc116d94405a245a060cd0fa91f94
XxShadowsmartxX <xxshadowsmartxx@gmail.com>
2020-07-29 17:38:47 -0500

    Update phase1.js

--- commands/phase1.js
+++ commands/phase1.js
@@ -10,7 +10,7 @@ module.exports = {
             .setColor('020f73')
             .setTitle('Phase 1 Notification')
             .setAuthor('ALETA Phase System', 'https://media.discordapp.net/attachments/705093565113434212/736015262477844500/6c1e7537f9aa230b0a49494c49779dca.png')
-            .setDescription(`hi`)
+            .setDescription(`A **Phase #** is being hosted right now.\n\nPhase is being hosted by: ${message.author}\n\nPhase is being slocked at: **${message.content}**\n\n**Campus Link:** https://www.roblox.com/games/5046852624/ALETA-Campus?refPageId=703da718-c993-4ed5-b154-4b2f3017c2ba#`)
         message.channel.send(embed);
             message.delete()
 
