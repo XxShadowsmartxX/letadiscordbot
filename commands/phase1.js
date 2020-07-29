c27025c6ed229b4689f988ac6b69b6a066b62785
XxShadowsmartxX <xxshadowsmartxx@gmail.com>
2020-07-29 17:21:36 -0500

    Update phase1.js

--- commands/phase1.js
+++ commands/phase1.js
@@ -10,8 +10,7 @@ module.exports = {
             .setColor('020f73')
             .setTitle('Phase 1 Notification')
             .setAuthor('ALETA Phase System', 'https://media.discordapp.net/attachments/705093565113434212/736015262477844500/6c1e7537f9aa230b0a49494c49779dca.png')
-            .setDescription(`A **Phase 1** is being hosted right now.\n\n**Phase Host: ${message.author}\n\nPhase Slock At: **${args[0]} ${args[1]} ${args[2]}**\n\n- Make a divisional outside of the campus on the yellow road lines.\n- PTS is in effect.\n- Follow all directions given by the hosts and co-hosts.\n- Be prepared and pay attention!``\n\n**Campus Link**: https://www.roblox.com/games/5046852624/ALETA-Campus?refPageId=703da718-c993-4ed5-b154-4b2f3017c2ba#`)
-            .setFooter('Aigio Law Enforcement Training Academy', 'https://media.discordapp.net/attachments/705093565113434212/736015262477844500/6c1e7537f9aa230b0a49494c49779dca.png')
+            .setDescription(`hi`)
         message.channel.send(embed);
             message.delete()
 
