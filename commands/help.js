501f4c11bb78a09de19a68f06c738b1e1e19012b
XxShadowsmartxX <xxshadowsmartxx@gmail.com>
2020-07-30 15:12:28 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 module.exports = {
-    name: 'help', 
+    name: 'support', 
     description: "this is a youtube command!", 
     execute(message, args){
         message.delete()
