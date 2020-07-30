868449e858fde5dae8aec15ebeb28f45822b1b71
XxShadowsmartxX <xxshadowsmartxx@gmail.com>
2020-07-30 14:25:57 -0500

    Update phasesl.js

--- commands/phasesl.js
+++ commands/phasesl.js
@@ -4,6 +4,7 @@ module.exports = {
     name: 'phasesl', 
     description: "this is a youtube command!", 
     execute(message, args){
+        message.delete()
         if (message.member.roles.cache.has('705082980770512996')){
         const embed = new Discord.MessageEmbed()
             .setColor('020f73')
@@ -11,11 +12,9 @@ module.exports = {
             .setAuthor('ALETA Phase System', 'https://media.discordapp.net/attachments/705093565113434212/736015262477844500/6c1e7537f9aa230b0a49494c49779dca.png')
             .setDescription(`**This phase has been slocked!**\n\nMore phases will be hosted in the future by the LETA Instructor Team.`)
         message.channel.send(embed);
-            message.delete()
 
         } else {
             message.channel.send('Insufficient Permissions.');
-            message.delete()
         }
 
     }
