657ff3f7dbcc0a46795b5c759e53f3294dcb5f63
XxShadowsmartxX <xxshadowsmartxx@gmail.com>
2020-07-30 16:02:34 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -12,7 +12,7 @@ module.exports = {
             .setTitle('All Commands')
             .setAuthor('ALETA Help Command', 'https://media.discordapp.net/attachments/705093565113434212/736015262477844500/6c1e7537f9aa230b0a49494c49779dca.png')
             .setDescription(`**-phase1** TIME OF SLOCK AM/PM TIMEZONE\n**Ex.** -phase1 11:45 PM PST\n\n**-phase2** TIME OF SLOCK AM/PM TIMEZONE\n**Ex.** -phase2 11:45 PM PST\n\n**-phase3** TIME OF SLOCK AM/PM TIMEZONE\n**Ex.** -phase3 11:45 PM PST\n\n**-phase4** TIME OF SLOCK AM/PM TIMEZONE\n**Ex.** -phase4 11:45 PM PST\n\n**-phasesl** - Slock command\n**Ex.** -phasesl\n\n**-phasecl** SCREENSHOT LINK\n**Ex.** -phasecl https://gyazo.com/9bf124b58ea8348fd043a795584b9ab4`)
-        message.channel.send(embed);
+        message.author.send(embed);
 
         } else {
             message.channel.send('Insufficient Permissions.');
