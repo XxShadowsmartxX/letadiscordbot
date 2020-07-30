e60a0c54fe9a3377435188874d8fe9bac5745d15
XxShadowsmartxX <xxshadowsmartxx@gmail.com>
2020-07-30 16:18:19 -0500

    Update phase2cl.js

--- commands/phase2cl.js
+++ commands/phase2cl.js
@@ -6,6 +6,7 @@ module.exports = {
     execute(message, args){
         message.delete()
         if (message.member.roles.cache.has('705082980770512996')){
+            message.channel.send('<@&705083000118837268>')
         const embed = new Discord.MessageEmbed()
             .setColor('020f73')
             .setTitle('Phase 2 Conclusion Notification')
