aeeb0eb3ddcb70c43b6cd6aac637ec63d4044914
XxShadowsmartxX <xxshadowsmartxx@gmail.com>
2020-07-30 14:47:20 -0500

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'help', 
+    description: "this is a youtube command!", 
+    execute(message, args){
+        message.delete()
+        if (message.member.roles.cache.has('705092415899303977')){
+            message.channel.send(`${message.author}`)
+        const embed = new Discord.MessageEmbed()
+            .setColor('020f73')
+            .setTitle('Help Command')
+            .setAuthor('ALETA Command System', 'https://media.discordapp.net/attachments/705093565113434212/736015262477844500/6c1e7537f9aa230b0a49494c49779dca.png')
+            .setDescription(`-phase1 TIME OF SLOCK AM/PM TIMEZONE\nEx. -phase1 11:45 PM PST\n\n-phase2 TIME OF SLOCK AM/PM TIMEZONE\nEx. -phase2 11:45 PM PST\n\n-phase3 TIME OF SLOCK AM/PM TIMEZONE\nEx. -phase3 11:45 PM PST\n\n-phase4 TIME OF SLOCK AM/PM TIMEZONE\nEx. -phase4 11:45 PM PST\n\n-phasesl - Slock command\n\n-phasecl SCREENSHOT LINK\nEx. -phasecl https://gyazo.com/9bf124b58ea8348fd043a795584b9ab4`)
+        message.channel.send(embed);
+
+        } else {
+            message.channel.send('Insufficient Permissions.');
+        }
+
+    }
+    }
