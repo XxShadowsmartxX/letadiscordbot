5b97df9948b4c117d54053bbe566bbda4190e2ff
XxShadowsmartxX <xxshadowsmartxx@gmail.com>
2020-07-30 14:25:50 -0500

    Update phase2.js

--- commands/phase2.js
+++ commands/phase2.js
@@ -4,6 +4,7 @@ module.exports = {
     name: 'phase2', 
     description: "this is a youtube command!", 
     execute(message, args){
+        message.delete()
         if (message.member.roles.cache.has('705082980770512996')){
             message.channel.send('<@&705083000118837268> <@&705082979784851596>')
         const embed = new Discord.MessageEmbed()
@@ -12,11 +13,9 @@ module.exports = {
             .setAuthor('ALETA Phase System', 'https://media.discordapp.net/attachments/705093565113434212/736015262477844500/6c1e7537f9aa230b0a49494c49779dca.png')
             .setDescription(`A **Phase 2** is being hosted at the campus!\n\nDivisional at the yellow road lines. Grab a uniform if needed. PTS is in effect.\n\n**Phase 2 Host:** ${message.author}\n\n**Starting Time:** ${args[0]} ${args[1]} ${args[2]}\n\n**Campus Link:** https://www.roblox.com/games/5046852624/ALETA-Campus?refPageId=703da718-c993-4ed5-b154-4b2f3017c2ba#`)
         message.channel.send(embed);
-            message.delete()
 
         } else {
             message.channel.send('Insufficient Permissions.');
-            message.delete()
         }
 
     }
