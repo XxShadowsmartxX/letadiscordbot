cfb6de9923ba5a65b9b1f24859b501fd1f2fcf8f
XxShadowsmartxX <xxshadowsmartxx@gmail.com>
2020-07-30 16:15:07 -0500

    Create phase4cl.js

---
+++ commands/phase4cl.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'phase4cl', 
+    description: "this is a youtube command!", 
+    execute(message, args){
+        message.delete()
+        if (message.member.roles.cache.has('705082980770512996')){
+        const embed = new Discord.MessageEmbed()
+            .setColor('020f73')
+            .setTitle('Phase 4 Conclusion Notification')
+            .setAuthor('ALETA Phase System', 'https://media.discordapp.net/attachments/705093565113434212/736015262477844500/6c1e7537f9aa230b0a49494c49779dca.png')
+            .setDescription(`**This phase 4** has **concluded!**\n\n**Phase Host:** ${message.author}\n\nMake sure to take the phase examination and complete it fully. Take your time.\n\n**Evidence of Phase:** ${args[0]}\n\n**Examination Link:** https://docs.google.com/forms/d/1y997jxRHIVipY7dlXpi5vz9uQdrFP_yqQKQAnl9vKR8/edit`)
+        message.channel.send(embed);
+
+        } else {
+            message.channel.send('Insufficient Permissions.');
+        }
+    }
+    }
