c799ce4ce98fd2df3ff29d42e0e46022a904ff3f
XxShadowsmartxX <xxshadowsmartxx@gmail.com>
2020-07-29 19:05:25 -0500

    Create phasesl.js

---
+++ commands/phasesl.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'phasesl', 
+    description: "this is a youtube command!", 
+    execute(message, args){
+        message.channel.send('<@&716370077611720746>');
+        if (message.member.roles.cache.has('716371210023338027')){
+        const embed = new Discord.MessageEmbed()
+            .setColor('020f73')
+            .setTitle('Phase Slock Notification')
+            .setAuthor('ALETA Phase System', 'https://media.discordapp.net/attachments/705093565113434212/736015262477844500/6c1e7537f9aa230b0a49494c49779dca.png')
+            .setDescription(`**THIS PHASE HAS BEEN SLOCKED**\n\nThe phase has been slocked at this time. If you missed the phase, there will be more hosted by the LETA Instructor Team.`)
+        message.channel.send(embed);
+            message.delete()
+
+        } else {
+            message.channel.send('Insufficient Permissions.');
+            message.delete()
+        }
+
+    }
+    }
