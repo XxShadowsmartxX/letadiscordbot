bfa0671114ef6454daaa3b7d2aad85f4fa16cdbb
XxShadowsmartxX <xxshadowsmartxx@gmail.com>
2020-08-01 19:39:29 -0500

    Update phasesl.js

--- commands/phasesl.js
+++ commands/phasesl.js
@@ -10,7 +10,7 @@ module.exports = {
             .setColor('020f73')
             .setTitle('Phase Slock Notification')
             .setAuthor('ALETA Phase System', 'https://media.discordapp.net/attachments/705093565113434212/736015262477844500/6c1e7537f9aa230b0a49494c49779dca.png')
-            .setDescription(`**This phase has been slocked!**\n\nMore phases will be hosted in the future by the LETA Instructor Team.`)
+            .setDescription(`**This phase has been slocked ${time}!**\n\nMore phases will be hosted in the future by the LETA Instructor Team.`)
         message.channel.send(embed);
 
         } else {
