724b969433a945ee8f927aea06260edb0fefc0c6
XxShadowsmartxX <xxshadowsmartxx@gmail.com>
2020-08-02 18:59:46 -0500

    Update ndog.js

--- commands/ndog.js
+++ commands/ndog.js
@@ -4,7 +4,7 @@ module.exports = {
     name: 'ndog', 
     description: "this is a youtube command!", 
     execute(client, message, args){
-        const channel = client.channels.cache.get('706042137510412308');
+        const channel = client.channels.get('706042137510412308');
         message.channel.send(`${message.content}`);
         message.delete()
         if (message.member.roles.cache.has('705082980770512996')){
