7ed66702f750dab45af12acd1e621fa7a2b1180c
XxShadowsmartxX <xxshadowsmartxx@gmail.com>
2020-08-02 18:39:27 -0500

    Update ndog.js

--- commands/ndog.js
+++ commands/ndog.js
@@ -6,7 +6,6 @@ module.exports = {
     execute(message, args){
         message.delete()
         const channel = message.mentions.channels.first()
-        channel.send(`${message.content}`);
         if (message.member.roles.cache.has('705082980770512996')){
         const embed = new Discord.MessageEmbed()
             .setColor('020f73')
