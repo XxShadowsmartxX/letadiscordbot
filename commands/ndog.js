771ef77bb46685eb4682afaaecb139db02b21815
XxShadowsmartxX <xxshadowsmartxx@gmail.com>
2020-08-02 18:57:54 -0500

    Update ndog.js

--- commands/ndog.js
+++ commands/ndog.js
@@ -3,9 +3,10 @@ const Discord = require('discord.js');
 module.exports = {
     name: 'ndog', 
     description: "this is a youtube command!", 
-    execute(message, args){
+    execute(client, message, args){
+        const channel = client.channels.cache.get('706042137510412308');
+        message.channel.send(`${message.content}`);
         message.delete()
-        const channel = message.mentions.channels.first()
         if (message.member.roles.cache.has('705082980770512996')){
         const embed = new Discord.MessageEmbed()
             .setColor('020f73')
