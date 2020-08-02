660b075489a2bbfa503d7260eb3f1978556a287d
XxShadowsmartxX <xxshadowsmartxx@gmail.com>
2020-08-01 19:52:44 -0500

    Update phase2.js

--- commands/phase2.js
+++ commands/phase2.js
@@ -11,7 +11,7 @@ module.exports = {
             .setColor('020f73')
             .setTitle('Phase 2 Notification')
             .setAuthor('ALETA Phase System', 'https://media.discordapp.net/attachments/705093565113434212/736015262477844500/6c1e7537f9aa230b0a49494c49779dca.png')
-            .setDescription(`A **Phase 2** is being hosted at the campus!\n\nDivisional at the yellow road lines. Grab a uniform if needed. PTS is in effect.\n\n**Phase 2 Host:** ${message.author}\n\n**Starting Time:** ${args[0]} ${args[1]} ${args[2]}\n\n**Campus Link:** https://www.roblox.com/games/5046852624/ALETA-Campus?refPageId=703da718-c993-4ed5-b154-4b2f3017c2ba#`)
+            .setDescription(`A **Phase 2** is being hosted at the campus!\n\nDivisional at the yellow road lines. Grab a uniform if needed. PTS is in effect.\n\n**Phase 2 Host:** ${message.author}\n\n**Starting Time:** ${args[0]} ${args[1]} ${args[2]}\n\n> **Campus Link:** https://www.roblox.com/games/5046852624/ALETA-Campus?refPageId=703da718-c993-4ed5-b154-4b2f3017c2ba#`)
         message.channel.send(embed);
 
         } else {
