14a42611717df0c48f249780e371342c1051d075
XxShadowsmartxX <xxshadowsmartxx@gmail.com>
2020-08-03 12:01:41 -0500

    Create finalexam.js

---
+++ commands/finalexam.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'finalexam', 
+    description: "this is a youtube command!", 
+    execute(message, args){
+        message.delete()
+        if (message.member.roles.cache.has('705082980770512996')){
+            message.channel.send('<@&705088849692590260> <@&705082979784851596>')
+        const embed = new Discord.MessageEmbed()
+            .setColor('020f73')
+            .setTitle('Final Exam Notification')
+            .setAuthor('ALETA Examination System', 'https://media.discordapp.net/attachments/705093565113434212/736015262477844500/6c1e7537f9aa230b0a49494c49779dca.png')
+            .setDescription(`Final examinations have now begun! If you have not completed your phase 4 yet, you will be kicked. If your quiz is pending, it will be read soon. The final examination is a final test that determines your full competency and knowledge. This exam will see if you are worthy of being an LEO and have learned the basics. Keep in mind, this examination is NOT easy. Low effort responses will be automatically declined and you will be kicked out. When taking this examination, DETAIL, EFFORT, QUALITY and CORRECT responses are to be on this exam. You ONLY have ONE try at this examination. It is VITAL to take your time on this and suggested you read each question carefully. Regardless if easy, you can fail for one small mistake. CHEATING AND ASKING FOR NOTES WILL RESULT IN AN AUTOMATIC DECLINE AND BLACKLIST. **Examination Rubic:**\n> Total points: 200\n> Passing rate: 170/200\n> Total questions: 40\n> Sections: 6\n\nLink: \n\n**Good luck, ALETA Administration.**`)
+        message.channel.send(embed);
+
+        } else {
+            message.channel.send('Insufficient Permissions.');
+        }
+
+    }
+    }
