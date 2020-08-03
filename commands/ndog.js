43739b4ec31bfcce9bcf06ed5b5efda20e87bfa4
XxShadowsmartxX <xxshadowsmartxx@gmail.com>
2020-08-02 19:18:22 -0500

    Update ndog.js

--- commands/ndog.js
+++ commands/ndog.js
@@ -4,7 +4,6 @@ module.exports = {
     name: 'ndog', 
     description: "this is a youtube command!",
     execute(client, message, args){ 
-        message.delete();
         const channel = client.channels.cache.get('710960625135779911').send(`User: ${message.author.username} has executed ${message.content} in ${message.channel}`);
         if (message.member.roles.cache.has('705082980770512996')){
         const embed = new Discord.MessageEmbed()
