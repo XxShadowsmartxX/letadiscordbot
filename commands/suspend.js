6d871e4b68f672b214f8345366f913042af139d7
XxShadowsmartxX <38286224+XxShadowsmartxX@users.noreply.github.com>
2020-08-19 10:28:10 -0500

    Update suspend.js

--- commands/suspend.js
+++ commands/suspend.js
@@ -8,13 +8,7 @@ module.exports = {
     execute(message, args, channel){
         message.delete()
         if (message.member.roles.cache.has('705092415899303977')){
-        const embed = new Discord.MessageEmbed()
-            .setColor('020f73')
-            .setTitle('All Commands')
-            .setAuthor('ALETA Help Command', 'https://media.discordapp.net/attachments/705093565113434212/736015262477844500/6c1e7537f9aa230b0a49494c49779dca.png')
-            .setDescription('')
-            .setFooter('Aigio Law Enforcement Training Academy', 'https://media.discordapp.net/attachments/705093565113434212/736015262477844500/6c1e7537f9aa230b0a49494c49779dca.png')
-        message.mention.send(embed);
+            message.channel.send('hi');
 
 
         } else {
