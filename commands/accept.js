4b2c66b794fadb5694f3831548025c3c6ed0054c
XxShadowsmartxX <38286224+XxShadowsmartxX@users.noreply.github.com>
2020-08-27 16:40:12 -0500

    Update accept.js

--- commands/accept.js
+++ commands/accept.js
@@ -6,8 +6,7 @@ module.exports = {
     execute(message, args){
         message.delete()
         if (message.member.roles.cache.has('705082977209548870')){
-            const user = client.users.cache.get();
-            user.send('testing');
+            message.user.mention.send('hi')
 
         } else {
             message.channel.send('Insufficient Permissions.');
