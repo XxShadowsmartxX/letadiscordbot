22aa71ff554ac3d16c2022f233b32a0c9dfa7cfd
XxShadowsmartxX <38286224+XxShadowsmartxX@users.noreply.github.com>
2020-08-27 15:56:31 -0500

    Update accept.js

--- commands/accept.js
+++ commands/accept.js
@@ -1,6 +1,4 @@
 const Discord = require('discord.js');
-const user = client.users.cache.get();
-user.send('testing');
 
 module.exports = {
     name: 'accept', 
@@ -8,13 +6,8 @@ module.exports = {
     execute(message, args){
         message.delete()
         if (message.member.roles.cache.has('705082977209548870')){
-            message.channel.send('<@&705082980770512996>')
-        const embed = new Discord.MessageEmbed()
-            .setColor('020f73')
-            .setTitle('Bot Maintenance Notification')
-            .setAuthor('ALETA Bot System', 'https://media.discordapp.net/attachments/705093565113434212/736015262477844500/6c1e7537f9aa230b0a49494c49779dca.png')
-            .setDescription(`**The ALETA Bot is undergoing some maintenance right now. It will be offline for a period of time.**\n\n__Under Maintenance By:__ ${message.author}\n\n> You will be notified when the bot is back online and ready for use again. `)
-        message.channel.send(embed);
+            const user = client.users.cache.get();
+            user.send('testing');
 
         } else {
             message.channel.send('Insufficient Permissions.');
