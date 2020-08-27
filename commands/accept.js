47e74aacc741fed1e512ddc52eb8cf10a11684d4
XxShadowsmartxX <38286224+XxShadowsmartxX@users.noreply.github.com>
2020-08-27 18:46:57 -0500

    Update accept.js

--- commands/accept.js
+++ commands/accept.js
@@ -7,8 +7,9 @@ module.exports = {
         message.delete()
         if (message.member.roles.cache.has('705082977209548870')){
             const msg = message.mentions.users.first();
-            message.guild.members.cache.get("id")
+            const msgg = message.guild.members.cache.get("id")
             msg.send('Your LETA certification has been revoked by the LETA Administration.')
+            msgg.send('testing')
             
 
         } else {
