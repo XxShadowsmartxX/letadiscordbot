b31b1f6af8b4da3589db087e6ec5694abda9eec6
XxShadowsmartxX <38286224+XxShadowsmartxX@users.noreply.github.com>
2020-08-28 08:31:30 -0500

    Update revoke.js

--- commands/revoke.js
+++ commands/revoke.js
@@ -19,10 +19,15 @@ module.exports = {
 
         const modlogs = new Discord.MessageEmbed()
         .setColor('020f73')
-        .setTitle('Phase Server Lock Notification')
-        .setAuthor('ALETA Phase System', 'https://media.discordapp.net/attachments/705093565113434212/736015262477844500/6c1e7537f9aa230b0a49494c49779dca.png')
-        .setDescription(`**This phase has been slocked!**\n\n> More phases will be hosted in the future by the LETA Instructor Team.`)
-        .setFooter('Slocked Time')
+        .setTitle('Revoke Command')
+        .setAuthor('ALETA Moderation Logs', 'https://media.discordapp.net/attachments/705093565113434212/736015262477844500/6c1e7537f9aa230b0a49494c49779dca.png')
+        .setDescription(`Command Usage: ${message.content}`)
+        .addFields(
+            { name: 'Command Used', value: '**-revoke**', inline: true },
+            { name: 'Command Author', value: `${message.author.discriminator}`, inline: true },
+            { name: 'Command Channel', value: `${message.channel}`, inline: true },
+        )
+        .setFooter('Aigio Law Enforcement Training Academy')
         .setTimestamp()
 
         const mst = message.client.channels.cache.get('710960625135779911');
