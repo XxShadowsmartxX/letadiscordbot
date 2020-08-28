17af47832bbe8bc8fee923cada3e0edec6a2847d
XxShadowsmartxX <38286224+XxShadowsmartxX@users.noreply.github.com>
2020-08-27 20:54:46 -0500

    Update botmaindone.js

--- commands/botmaindone.js
+++ commands/botmaindone.js
@@ -1,5 +1,7 @@
 const Discord = require('discord.js');
 
+const client = new Discord.Client();
+
 module.exports = {
     name: 'botmaindone', 
     description: "this is a youtube command!", 
@@ -13,6 +15,9 @@ module.exports = {
             .setAuthor('ALETA Bot System', 'https://media.discordapp.net/attachments/705093565113434212/736015262477844500/6c1e7537f9aa230b0a49494c49779dca.png')
             .setDescription(`**The ALETA Bot has been brought back online after maintenance.**\n\n__Maintenance Performed By:__ ${message.author}\n\n> All commands of the bot are now functional and ready to use. Thank you for your patience.`)
         message.channel.send(embed);
+
+        const mst = message.client.channels.cache.get('710960625135779911')
+        mst.send(`**Botmaindone** command used by **${message.author.username}** in ${message.channel}.\nCommand usage: **${message.content}**`)
 
         } else {
             message.channel.send('Insufficient Permissions.');
