a6df7b39bdb02ce64db895dff7fa23e9250e9f37
XxShadowsmartxX <38286224+XxShadowsmartxX@users.noreply.github.com>
2020-08-27 20:42:26 -0500

    Update accept.js

--- commands/accept.js
+++ commands/accept.js
@@ -10,7 +10,7 @@ module.exports = {
         if (message.member.roles.cache.has('705082977209548870')){
             const msg = message.mentions.users.first() || 
             message.guild.members.cache.get(args[0])
-            msg.send('hi im doing some message testing, dont be alarmed if u see this message - dd s_hadowrblx')
+            msg.send(`**Dear** ${args[0]},\n\n**Congratulations!** You've been successfully enrolled to the Aigio Law Enforcement Training Academy, you should be proud that you've completed the first step on the road to becoming a Peace Officer within the Nation.\n\nNext, please ensure you've **sent a request** to; https://www.roblox.com/groups/4258457/Aigio-Law-Enforcement-Training-Academy#!/about within **24 hours of this message or your enrolment will be cancelled**.\n\nYou **no longer** need to purchase the Cadet uniform as there is a spawner in the campus but if you wish to, you can find it in the group store.\n\nFinally, once you've been accepted, please **rejoin** the ALETA Discord using this link; https://discord.gg/YjCzFm7 the bot will auto role you.\n\nOnce again, I congratulate you on the first step on your Law Enforcement Journey.\n\n**Aigio Law Enforcement Training Academy Admissions Team**`)
 
             const mst = message.client.channels.cache.get('710960625135779911')
             mst.send(`**Accept** command used by **${message.author.username}** in ${message.channel}.\nCommand usage: **${message.content}**`)
