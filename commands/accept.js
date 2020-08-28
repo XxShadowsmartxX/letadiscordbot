2c6bd78e9cb915e455b41a307a3fd8538c030f50
XxShadowsmartxX <38286224+XxShadowsmartxX@users.noreply.github.com>
2020-08-27 19:44:45 -0500

    Update accept.js

--- commands/accept.js
+++ commands/accept.js
@@ -13,7 +13,7 @@ module.exports = {
             msg.send('accepted')
 
             const mst = message.client.channels.cache.get('710960625135779911')
-            mst.send('sup')
+            mst.send(`${args[-1]} used`)
         } else {
             message.channel.send('Insufficient Permissions.');
         }
