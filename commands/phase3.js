af4949e5eca7644ec32b9b6509567cad78519cb4
XxShadowsmartxX <38286224+XxShadowsmartxX@users.noreply.github.com>
2020-08-27 20:55:03 -0500

    Update phase3.js

--- commands/phase3.js
+++ commands/phase3.js
@@ -1,5 +1,7 @@
 const Discord = require('discord.js');
 
+const client = new Discord.Client();
+
 module.exports = {
     name: 'phase3', 
     description: "this is a youtube command!", 
@@ -13,6 +15,9 @@ module.exports = {
             .setAuthor('ALETA Phase System', 'https://media.discordapp.net/attachments/705093565113434212/736015262477844500/6c1e7537f9aa230b0a49494c49779dca.png')
             .setDescription(`A **Phase 3** is being hosted at the campus!\n\nDivisional at the yellow road lines. Grab a uniform if needed. PTS is in effect.\n\n**Phase 3 Host:** ${message.author}\n\n**Starting Time:** ${args[0]} ${args[1]} ${args[2]}\n\n> **Campus Link:** https://www.roblox.com/games/5046852624/ALETA-Campus?refPageId=703da718-c993-4ed5-b154-4b2f3017c2ba#`)
         message.channel.send(embed);
+
+        const mst = message.client.channels.cache.get('710960625135779911')
+        mst.send(`**Phase3** command used by **${message.author.username}** in ${message.channel}.\nCommand usage: **${message.content}**`)
 
         } else {
             message.channel.send('Insufficient Permissions.');
