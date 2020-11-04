2608d3da81ff2dce40fd0f11aa84c2d22477eceb
XxShadowsmartxX <38286224+XxShadowsmartxX@users.noreply.github.com>
2020-11-04 11:59:57 -0600

    done

--- commands/phase2.js
+++ commands/phase2.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
+
 module.exports = {
     name: 'phase2', 
     description: "this is a youtube command!", 
--- main.js
+++ main.js
@@ -88,8 +88,8 @@ client.on('message', message =>{
     client.commands.get('checkwarns').execute(message, args);
 } else if (command === 'slowmode'){
     client.commands.get('slowmode').execute(message, args);
-} else if (command === 'delete'){
-    client.commands.get('delete').execute(message, args);
+} else if (command === 'poll'){
+    client.commands.get('poll').execute(message, args);
 } else if (command === 'kick'){
     client.commands.get('kick').execute(message, args);
 } else if (command === 'logs'){
